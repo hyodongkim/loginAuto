11019eb4bfe7833b68e3ec311231adb261580538
hyodongkim <kimhd853@naver.com>
2024-01-31 00:47:23 +0900

    go3

--- modules/pages/main.js
+++ modules/pages/main.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/home', (req,res,next)=>{
-    res.locals.user = req.user;
-    res.render('home');
+router.get("/home", (req, res, next) => {
+  res.locals.user = req.user;
+  res.render("home");
 });
-router.get('/login',require('../role/user'), (req,res,next)=>{
-    if(req.user) next(new Error());
-    else res.render('login');
+// router.get('/login',require('../role/user'), (req,res,next)=>{
+router.get("/login", require("../role/guest"), (req, res, next) => {
+  if (req.user) next(new Error());
+  else res.render("login");
 });
-router.get('/signup',require('../role/guest'), (req,res,next)=>{
-    if(req.user) next(new Error());
-    else res.render('signup');
+router.get("/signup", require("../role/guest"), (req, res, next) => {
+  if (req.user) next(new Error());
+  else res.render("signup");
 });
-router.get('/boardwrite',require('../role/user'), (req,res,next)=>{
-    
-    res.render('boardwrite');
+router.get("/boardwrite", require("../role/user"), (req, res, next) => {
+  res.render("boardwrite");
 });
-
 
 module.exports = router;
