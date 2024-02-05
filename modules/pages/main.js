0f3da0499ced8ff43f5a76001eb5af6f20aa0c08
hyodongkim <kimhd853@naver.com>
2024-02-05 21:19:49 +0900

    aaaa

--- modules/pages/main.js
+++ modules/pages/main.js
@@ -1,7 +1,57 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/home', require('../role/any'), (req, res, next)=>{
+router.get('/nextAds', require('../role/any'), async (req,res,next)=>{
+    let video;
+    if(req.session.leftVideos){
+        let results = req.session.leftVideos;
+        let index = parseInt(Math.random() * results.length);
+        video = results[index];
+        delete results[index];
+        results = results.filter(r=>r);
+        if(results.length < 1){
+            results = await (await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyAB7jesp2Et9CF32yNtPuNdB2LySYGmoL4&playlistId=PLvagogV4avthbgsEv70XpQp_Y9i5XEAgU&maxResults=50', { method:'get' })).json();
+            results = results.items.map(e=>e.snippet.resourceId.videoId);
+        }
+        req.session.leftVideos = results;
+    }
+    else{
+        let results = await (await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyAB7jesp2Et9CF32yNtPuNdB2LySYGmoL4&playlistId=PLvagogV4avthbgsEv70XpQp_Y9i5XEAgU&maxResults=50', { method:'get' })).json();
+        results = results.items.map(e=>e.snippet.resourceId.videoId);
+        let index = parseInt(Math.random() * results.length);
+        video = results[index];
+        delete results[index];
+        results = results.filter(r=>r);
+        req.session.leftVideos = results;
+    }
+    res.send(video);
+});
+
+router.get('/home', require('../role/any'), async (req, res, next)=>{
+    let video;
+    if(req.session.leftVideos){
+        let results = req.session.leftVideos;
+        let index = parseInt(Math.random() * results.length);
+        video = results[index];
+        delete results[index];
+        results = results.filter(r=>r);
+        if(results.length < 1){
+            results = await (await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyAB7jesp2Et9CF32yNtPuNdB2LySYGmoL4&playlistId=PLvagogV4avthbgsEv70XpQp_Y9i5XEAgU&maxResults=50', { method:'get' })).json();
+            results = results.items.map(e=>e.snippet.resourceId.videoId);
+        }
+        req.session.leftVideos = results;
+    }
+    else{
+        let results = await (await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyAB7jesp2Et9CF32yNtPuNdB2LySYGmoL4&playlistId=PLvagogV4avthbgsEv70XpQp_Y9i5XEAgU&maxResults=50', { method:'get' })).json();
+        results = results.items.map(e=>e.snippet.resourceId.videoId);
+        let index = parseInt(Math.random() * results.length);
+        video = results[index];
+        delete results[index];
+        results = results.filter(r=>r);
+        req.session.leftVideos = results;
+    }
+    res.locals.startVideo = video;
+
     res.render('home');
 });
 
@@ -17,12 +67,16 @@ router.get('/boards', require('../role/any'), async (req,res,next)=>{
     page = req.query.page || 1;
     keyword = req.query.keyword || "";
     let boards = await req.mongo.board.find({
-        title:{
-            $regex:new RegExp(`.*${keyword}.*`,'i')
-        },
-        inner:{
-            $regex:new RegExp(`.*${keyword}.*`,'i')
-    }}).sort({_id:-1}).skip((page - 1) * parseInt(process.env.PAGE_LIMIT)).limit(parseInt(process.env.PAGE_LIMIT));
+                title:{
+                    $regex:new RegExp(`.*${keyword}.*`,'i')
+                },
+                inner:{
+                    $regex:new RegExp(`.*${keyword}.*`,'i')
+                },
+            })
+            .sort({_id:-1})
+            .skip((page - 1) * parseInt(process.env.PAGE_LIMIT))
+            .limit(parseInt(process.env.PAGE_LIMIT));
     let now = new Date();
     now.setHours(now.getHours() - 24);
     let dateformat = date=>{
@@ -37,13 +91,12 @@ router.get('/boards', require('../role/any'), async (req,res,next)=>{
             `${date.getMinutes().toString().padStart(2,'0')}`;
     };
     let count = await req.mongo.board.countDocuments({
-        title:{
-            $regex:new RegExp(`.*${keyword}.*`,'i')
-        },
-        inner:{
-            $regex:new RegExp(`.*${keyword}.*`,'i')
-        }
-    });
+                            title:{
+                                $regex:new RegExp(`.*${keyword}.*`,'i')
+                            },
+                            inner:{
+                                $regex:new RegExp(`.*${keyword}.*`,'i')
+                            }});
     let pageCount = Math.ceil(count / parseInt(process.env.PAGE_LIMIT));
     let begin = page - parseInt(process.env.PAGE_PADDING) < 1 ? 
         1 : page - parseInt(process.env.PAGE_PADDING);
@@ -59,6 +112,7 @@ router.get('/boards', require('../role/any'), async (req,res,next)=>{
         begin:begin,
         end:end,
         last:pageCount,
+        keyword:keyword,
         dateformat:dateformat,
         splittoken:process.env.MEDIAPATH_SPLIT_TOKEN
     };
@@ -82,7 +136,7 @@ router.get('/board/:id', require('../role/any'), async (req,res,next)=>{
                         <span>${reply.inner}</span>
                     </summary>
                     ${replysfn(replys.filter(r=>reply._id.equals(r.ownreply)), lpad)}
-                    <form action="${`/board/${req.params.id}`}" method="post">
+                    <form action="${`/board/${req.params.id}`}${`?page=${page}${keyword ? `&keyword=${keyword}` : ""}`}" method="post">
                         <input type="text" hidden name="own" value="${reply._id}">
                         <input type="text" name="reply">
                         <button type="submit">등록</button>
@@ -96,6 +150,7 @@ router.get('/board/:id', require('../role/any'), async (req,res,next)=>{
     res.render("board", {
         board:board, 
         page:req.query.page,
+        keywrod:req.query.keyword,
         id:req.params.id, 
         replys:replysfn(replys.filter(r=>!r.ownreply), 20),
         splittoken:process.env.MEDIAPATH_SPLIT_TOKEN
